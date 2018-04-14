a6f56809d2b5cc9da93c53a475a813e9a5ee06ab
aimee223 <35387935+aimee223@users.noreply.github.com>
2018-04-13 21:47:32 -0700

    Created The Mountain Spoke, my CIT230 term project!

---
+++ scripts/jquery.js
@@ -0,0 +1,17 @@
+$(function() {
+    $("section.chic h2").click(function() {
+        $("article.chicinfo").slideToggle(500);
+    });
+});
+
+$(function() {
+    $("section.retro h2").click(function() {
+        $("article.retroinfo").slideToggle(500);
+    });
+});
+
+$(function() {
+    $("section.tyke h2").click(function() {
+        $("article.tykeinfo").slideToggle(500);
+    });
+});
